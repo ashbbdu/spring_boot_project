7015baa2696f4fba0f99d7db39da161af95fcb88
ashbbdu <ashishsrivastava7007@gmail.com>
2026-04-12 23:40:21 +0530

    inital push

---
+++ src/main/java/com/zappgo/ashish/module1_demo/Module1DemoApplication.java
@@ -0,0 +1,14 @@
+package com.zappgo.ashish.module1_demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1DemoApplication {
+//	pri@Efeed6392
+	public static void main(String[] args) {
+		SpringApplication.run(Module1DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zappgo/ashish/module1_demo/Module1DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zappgo.ashish.module1_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
